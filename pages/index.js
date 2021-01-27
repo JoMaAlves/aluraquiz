9ff9b890af7b805a67607272dd8d2c956b0e4243
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-01-27 00:32:25 -0300

    Fim da segunda aula de imersao

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 /* const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -24,27 +29,54 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push(`/quiz?name=${name}`);
+    console.log('Fazendo a submissao por meio do react');
+  };
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>JomaQuiz</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Full Metal Alchemist</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Diz ai seu nome"
+                onChange={handleChange}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-            <p>lorem ipsum dolor sit amet...</p> 
+            <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/JoMaAlves"/>
+      <GitHubCorner projectUrl="https://github.com/JoMaAlves" />
     </QuizBackground>
-  )
+  );
 }
