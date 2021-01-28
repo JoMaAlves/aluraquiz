d8d18df3763584e35a858e1bb579d85d40ea5fe3
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-01-28 02:27:12 -0300

    Fim da aula 3

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,6 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-
 import db from '../db.json';
-import Input from '../src/components/Input';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import Button from '../src/components/Button';
@@ -13,50 +9,135 @@ import GitHubCorner from '../src/components/GitHubCorner';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  totalQuestions,
+  questionIndex,
+  onSubmit,
+}) {
+  const questionId = `question_${questionIndex}`;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        src={question.image}
+        style={{
+          width: '100%',
+          height: 150,
+          objectFit: 'cover',
+        }}
+        alt="Question representation"
+      />
+
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+
+        <form onSubmit={handleSubmit}>
+          {question.alternatives.map((alternative, index) => {
+            const alternativeId = `alternative_${index}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button type="submit">Confirmar</Button>
+        </form>
+      </Widget.Content>
+
+    </Widget>
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
 export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const [currentQuention, setCurrentQuention] = React.useState(0);
+  const questionIndex = currentQuention;
+  const totalQuestions = db.questions.length;
+  const question = db.questions[questionIndex];
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1000);
+  }, []);
+
+  const handleSubmit = () => {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuention(questionIndex + 1);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  };
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            {/* <BackLinkArrow href="/" /> */}
-            <h3>
-              Pergunta
-              1
-              de
-              {` ${db.questions.length}`}
-            </h3>
-          </Widget.Header>
 
-          <img
-            src={db.bg}
-            style={{
-              width: '100%',
-              height: 150,
-              objectFit: 'cover',
-            }}
-            alt="Question representation"
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            onSubmit={handleSubmit}
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
           />
+        )}
 
-          <Widget.Content>
-            <h2>
-              Titulo
-            </h2>
-            <p>
-              Descrição
-            </p>
-            <form onSubmit="">
-              <button type="submit">Lorem ipsum</button>
-              <button type="submit">Lorem ipsum</button>
-              <button type="submit">Lorem ipsum</button>
-              <button type="submit">Lorem ipsum</button>
-            </form>
-          </Widget.Content>
-        </Widget>
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Voce acertou X questões, parabéns</div>}
+
         <Footer />
       </QuizContainer>
-      <GitHubCorner />
+
+      <GitHubCorner projectUrl="https://github.com/JoMaAlves" />
     </QuizBackground>
   );
 }
